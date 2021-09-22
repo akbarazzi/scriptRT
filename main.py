5b15c2d2f880f73a4ea9ffeb35e55edfac3c4368
Akbar Azzi <akbar.azzi@gmail.com>
2021-09-22 07:51:27 +0200

    main script

---
+++ main.py
@@ -0,0 +1,13 @@
+# phyton program for RT shielding
+# script on python
+
+import numpy as np
+from tkinter import *
+import pydicom
+
+window = Tk()
+
+label = Label(window, text="it is me").pack()
+
+
+window.mainloop()
